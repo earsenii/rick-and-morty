aa358af02fd63778d181e970200698cd2e3414f1
unknown <ermolarsen@mail.ru>
2021-09-19 22:28:01 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 min-w-full min-h-screen flex justify-center items-center space-x-7">
+      <div className="bg-white w-60 h-36 rounded-md flex justify-center items-center shadow-md cursor-pointer transform transition duration-500 hover:scale-110 filter grayscale hover:grayscale-0 overflow-hidden text-white text-2xl ">
+        <img
+          src="/img/characters.jpg"
+          alt="characters"
+          className="w-full h-full object-center object-cover filter brightness-50"
+        />
+        <div className="absolute font-semibold">Characters</div>
+      </div>
+      <div className="bg-white w-60 h-36 rounded-md flex justify-center items-center shadow-md cursor-pointer transform transition duration-500 hover:scale-110 filter grayscale hover:grayscale-0 overflow-hidden text-white text-2xl ">
+        <img
+          src="/img/episodes.jpg"
+          alt="episodes"
+          className="w-full h-full object-center object-cover filter brightness-50"
+        />
+        <div className="absolute font-semibold">Episodes</div>
+      </div>
+      <div className="bg-white w-60 h-36 rounded-md flex justify-center items-center shadow-md cursor-pointer transform transition duration-500 hover:scale-110 filter grayscale hover:grayscale-0 overflow-hidden text-white text-2xl ">
+        <img
+          src="/img/locations.jpg"
+          alt="locations"
+          className="w-full h-full object-center object-cover filter brightness-50"
+        />
+        <div className="absolute font-semibold">Locations</div>
+      </div>
     </div>
   );
 }
