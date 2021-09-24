1b2a3a3def435bb283b27c57c629c5f9649dfe2c
unknown <ermolarsen@mail.ru>
2021-09-24 17:56:19 +0300

    added more info about character

--- src/components/CharacterCardFull.jsx
+++ src/components/CharacterCardFull.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 
-function CharacterCardFull({ character, loading }) {
+function CharacterCardFull({ character }) {
+  const { name, status, species, gender, image, origin, location } = character;
+
   return (
-    <div className="overflow-hidden absolute flex rounded-md w-2/5 h-1/4 z-50 object-cover text-white bg-gray-600 cursor-default shadow-md">
+    <div className="overflow-hidden absolute flex rounded-md w-2/5 h-1/4 z-50 object-cover text-white bg-gray-700 cursor-default shadow-md">
       <div className="w-60 h-full object-center object-cover">
-        <img src={character.image} alt={character.name} />
+        <img src={image} alt={name} />
       </div>
-      <div className="pl-2 flex flex-col">
-        <div className="font-bold text-3xl">{character.name}</div>
+      <div className="ml-4 flex flex-col">
+        <div className="font-bold text-3xl">{name}</div>
         <div className="flex space-x-4 mt-4">
           <div className="flex flex-col -space-y-1.5">
             <div className="text-gray-300 text-md">Status</div>
-            <div className="font-semibold text-xl">{character.status}</div>
+            <div className="font-semibold text-xl">{status}</div>
           </div>
           <div className="flex flex-col -space-y-1.5">
             <div className="text-gray-300 text-md">Species</div>
-            <div className="font-semibold text-xl">{character.species}</div>
+            <div className="font-semibold text-xl">{species}</div>
           </div>
           <div className="flex flex-col -space-y-1.5">
             <div className="text-gray-300 text-md">Gender</div>
-            <div className="font-semibold text-xl">{character.gender}</div>
+            <div className="font-semibold text-xl">{gender}</div>
           </div>
+        </div>
+        <div className="flex flex-col -space-y-1.5 mt-4">
+          <div className="text-gray-300 text-md">Origin</div>
+          <div className="font-bold text-xl">{origin.name}</div>
+        </div>
+        <div className="flex flex-col -space-y-1.5 mt-4">
+          <div className="text-gray-300 text-md">Loaction</div>
+          <div className="font-bold text-xl">{location.name}</div>
         </div>
       </div>
     </div>
--- src/pages/CharacterFull.jsx
+++ src/pages/CharacterFull.jsx
@@ -6,16 +6,42 @@ function CharacterFull({ match }) {
   const {
     params: { id },
   } = match;
+  const [character, setCharacter] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState();
 
-  const [character, setCharacter] = React.useState([]);
-
-  React.useEffect(() => {
-    async function getCharacter() {
+  const getCharacter = async () => {
+    try {
       const result = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
       setCharacter(result.data);
+    } catch (error) {
+      setError(error);
     }
-    getCharacter();
+  };
+
+  React.useEffect(() => {
+    if (id) {
+      setLoading(false);
+      getCharacter();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
+
+  if (loading === true || !character) {
+    return (
+      <div className="bg-gray-850 min-w-full min-h-screen flex justify-center items-center space-x-7">
+        <h1 className="text-center text-white text-3xl">Loading</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="bg-gray-850 min-w-full min-h-screen flex justify-center items-center space-x-7">
+        <h1 className="text-center text-white text-3xl">{error.message}</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-gray-850 min-w-full min-h-screen flex justify-center">
