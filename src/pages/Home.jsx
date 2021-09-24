7fa0bc0f31c8076a684ee2e9180847e9a693750f
unknown <ermolarsen@mail.ru>
2021-09-24 19:13:10 +0300

    fixed Home Page styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import HomePageCard from '../components/HomePageCard';
 function Home() {
   return (
     <div className="bg-gray-850">
-      <div className="min-w-full min-h-screen flex justify-center items-center lg:flex-row lg:space-x-5 lg:space-y-0 md:flex-col md:space-y-5 sm:flex-col sm:space-y-3">
+      <div className="min-w-full min-h-screen flex flex-col space-y-3 justify-center items-center lg:flex-row lg:space-x-5 lg:space-y-0 md:space-y-5 sm:flex-col">
         <Link to="/characters">
           <HomePageCard text="Characters" image="/img/characters.jpg" />
         </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         gray: {
