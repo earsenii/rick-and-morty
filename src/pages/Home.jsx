9bc9eb92da02e536a0e75a14bbcf9a0cb9ed1b10
unknown <ermolarsen@mail.ru>
2021-09-24 20:13:33 +0300

    fixed styles HomePage and HomePageCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import HomePageCard from '../components/HomePageCard';
 function Home() {
   return (
     <div className="bg-gray-850">
-      <div className="min-w-full min-h-screen flex flex-col space-y-3 justify-center items-center lg:flex-row lg:space-x-5 lg:space-y-0 md:space-y-5 sm:flex-col">
+      <div className="min-w-full min-h-screen flex flex-col space-y-3 justify-start pt-5 items-center lg:justify-center lg:flex-row lg:space-x-5 lg:space-y-0 md:space-y-5 lg:pt-0 sm:flex-col">
         <Link to="/characters">
           <HomePageCard text="Characters" image="/img/characters.jpg" />
         </Link>
