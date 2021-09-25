b28bcf2a5e076f2104fae982556784f15ed6e743
unknown <ermolarsen@mail.ru>
2021-09-25 11:58:40 +0300

    deleted xs size from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    screens: {
-      xs: '475px',
-      ...defaultTheme.screens,
-    },
     height: {
-      245: '35rem',
-    },
-    width: {
       245: '35rem',
     },
     extend: {
