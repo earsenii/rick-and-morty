684f514b2c58e4a62e12b08f2d06f2fb19bbf57e
unknown <ermolarsen@mail.ru>
2021-09-27 19:29:15 +0300

    fixed sizes CharacterCardFull

--- src/components/CharacterCardFull.jsx
+++ src/components/CharacterCardFull.jsx
@@ -4,13 +4,13 @@ function CharacterCardFull({ character }) {
   const { name, status, species, gender, image, origin, location } = character;
 
   return (
-    <div className="overflow-hidden absolute flex flex-col rounded-md w-64 h-245 text-white bg-gray-825 cursor-default shadow-md ">
+    <div className="overflow-hidden absolute flex flex-col rounded-md w-64 h-auto text-white bg-gray-825 cursor-default shadow-md ">
       <div className="w-full p-2">
         <img src={image} alt={name} className="object-center object-cover rounded-md" />
       </div>
       <div className="flex flex-col p-2 w-full">
         <div className="font-bold text-2xl sm:text-3xl">{name}</div>
-        <div className="flex space-x-4 mt-4">
+        <div className="flex flex-col mt-4 space-y-2">
           <div className="flex flex-col -space-y-1.5">
             <div className="text-gray-300 text-sm sm:text-md">Status</div>
             <div className="font-semibold text-lg sm:text-xl">{status}</div>
@@ -23,14 +23,15 @@ function CharacterCardFull({ character }) {
             <div className="text-gray-300 text-sm sm:text-md">Gender</div>
             <div className="font-semibold text-lg sm:text-xl">{gender}</div>
           </div>
-        </div>
-        <div className="flex flex-col -space-y-1.5 mt-4">
-          <div className="text-gray-300 text-sm sm:text-md">Origin</div>
-          <div className="font-bold text-lg sm:text-xl">{origin.name}</div>
-        </div>
-        <div className="flex flex-col -space-y-1.5 mt-4">
-          <div className="text-gray-300 text-sm sm:text-md">Loaction</div>
-          <div className="font-bold text-lg sm:text-xl">{location.name}</div>
+
+          <div className="flex flex-col -space-y-1.5">
+            <div className="text-gray-300 text-sm sm:text-md">Origin</div>
+            <div className="font-bold text-lg sm:text-xl">{origin.name}</div>
+          </div>
+          <div className="flex flex-col -space-y-1.5">
+            <div className="text-gray-300 text-sm sm:text-md">Loaction</div>
+            <div className="font-bold text-lg sm:text-xl">{location.name}</div>
+          </div>
         </div>
       </div>
     </div>
