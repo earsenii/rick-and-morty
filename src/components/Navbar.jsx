5617acc2676d3313f055252faa4d4e2b4a4bceee
unknown <ermolarsen@mail.ru>
2021-09-27 18:58:46 +0300

    added mobile styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { HomeIcon, MapIcon, FilmIcon } from '@heroicons/react/solid';
+
 function Navbar() {
   return (
-    <div className="bg-gray-825 text-white absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 w-full shadow-md flex justify-center">
+    <div className="bg-gray-825 text-white transform md:relative md:translate-x-0 w-full shadow-md flex justify-center">
       <div className="w-3/5 flex flex-row justify-between items-center space-x-5">
-        <div className="flex flex-row justify-center space-x-12">
+        {/* Desktop */}
+        <div className="hidden md:flex flex-row justify-center space-x-5 lg:space-x-12">
           <Link
             to="/"
-            className="p-2 text-lg font-semibold rounded-lg transition duration-200 hover:bg-gray-850 ">
+            className="p-1 text-md lg:text-lg lg:p-2 font-semibold rounded-lg transition duration-200 hover:bg-gray-850 ">
             Home
           </Link>
           <Link
             to="/episodes"
-            className="p-2 text-lg font-semibold rounded-lg transition duration-200 hover:bg-gray-850 ">
+            className="p-1 text-md lg:text-lg lg:p-2 font-semibold rounded-lg transition duration-200 hover:bg-gray-850 ">
             Episodes
           </Link>
           <Link
             to="/locations"
-            className="p-2 text-lg  font-semibold rounded-lg transition duration-200 hover:bg-gray-850">
+            className="p-1 text-md lg:text-lg lg:p-2  font-semibold rounded-lg transition duration-200 hover:bg-gray-850">
             Locations
           </Link>
         </div>
-        <div className="w-60 py-2">
+        {/* Mobile */}
+        <div className="flex md:hidden flex-row justify-center space-x-2">
+          <Link to="/">
+            <HomeIcon className="h-6 w-6 text-white" />
+          </Link>
+          <Link to="/episodes">
+            <FilmIcon className="h-6 w-6 text-white" />
+          </Link>
+          <Link to="/locations">
+            <MapIcon className="h-6 w-6 text-white" />
+          </Link>
+        </div>
+        <div className="p-1 w-28 md:w-52 lg:w-60 lg:py-2">
           <input
             type="text"
             name="name"
-            placeholder="Search by name..."
-            className="w-full bg-gray-825 rounded-mg py-2 border-b-2 border-gray-500 text-md font-semibold outline-none focus:border-white pl-2"
+            placeholder="Search..."
+            className="w-full bg-gray-825 rounded-mg py-2 border-b-2 border-gray-500 text-md lg:text-lg font-semibold outline-none focus:border-white pl-2"
           />
         </div>
       </div>
