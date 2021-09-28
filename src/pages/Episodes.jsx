72d8a7f0a9e802b644800fc90502f4e97e5d3d8c
unknown <ermolarsen@mail.ru>
2021-09-28 20:27:06 +0300

    edited text on Episodes/Locations pages

--- src/pages/Episodes.jsx
+++ src/pages/Episodes.jsx
@@ -5,7 +5,9 @@ function Episodes() {
   return (
     <div className="bg-gray-850 min-w-full min-h-screen flex justify-center items-center space-x-7">
       <Navbar />
-      <h1 className="text-center text-white">Episodes</h1>
+      <h1 className="text-center text-white font-extrabold text-3xl md:text-5xl lg:text-7xl">
+        Will be soon or never...
+      </h1>
     </div>
   );
 }
