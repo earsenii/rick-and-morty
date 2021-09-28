c074f70b9bed6243f3c08a85b11f3ee98db05c16
unknown <ermolarsen@mail.ru>
2021-09-28 20:12:20 +0300

    fixed search characters

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -6,6 +6,7 @@ import Navbar from '../components/Navbar';
 
 function Characters() {
   const [character, setCharacter] = React.useState([]);
+  const [filtredCharacter, setFiltredCharacter] = React.useState([]);
   const [currentPage, setCurrentPage] = React.useState(1);
   const [fetching, setFetching] = React.useState(true);
   const [value, setValue] = React.useState('');
@@ -24,6 +25,16 @@ function Characters() {
     getCharacter();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetching]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const allCharacters = await axios.get(
+        `https://rickandmortyapi.com/api/character/?name=${value}`,
+      );
+      setFiltredCharacter(allCharacters.data.results);
+    }
+    fetchData();
+  }, [value]);
 
   React.useEffect(() => {
     document.addEventListener('scroll', scrollHandler);
@@ -46,7 +57,7 @@ function Characters() {
     setValue(event.target.value);
   };
 
-  const filtredCharacters = character.filter((item) =>
+  const filtredCharacters = filtredCharacter.filter((item) =>
     item.name.toLowerCase().includes(value.toLowerCase()),
   );
 
@@ -54,7 +65,11 @@ function Characters() {
     <div className="bg-gray-850 min-w-full min-h-screen flex flex-col justify-center items-center">
       <Navbar onChangeSearchInput={onChangeSearchInput} />
       <div className="w-3/4 lg:mt-20 mt-16 flex flex-col space-y-2 items-center justify-center flex-wrap md:flex-row md:items-start md:space-x-0 md:space-y-0 z-0">
-        <CharacterCardMini key={character.id} character={filtredCharacters} />
+        {value.length > 0 ? (
+          <CharacterCardMini key={character.id} character={filtredCharacters} />
+        ) : (
+          <CharacterCardMini key={character.id} character={character} />
+        )}
       </div>
     </div>
   );
